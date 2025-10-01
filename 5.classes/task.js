6432b270a1d71386c016ac454ebb8cd2a5e67353
Gala knd <g.kondratenko19@bk.ru>
2025-10-02 00:01:36 +0300

    Пятое задание полностью готово

---
+++ 5.classes/task.js
@@ -0,0 +1,186 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state *= 1.5;
+        if (this.state > 100) {
+            this.state = 100;
+        }
+    }
+
+    set state(value) {
+        if (value < 0) {
+            this._state = 0;
+        } else if (value > 100) {
+            this._state = 100;
+        } else {
+            this._state = value;
+        }
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'book';
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'novel';
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'fantastic';
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'detective';
+    }
+}
+
+const sherlock = new PrintEditionItem(
+    "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+    2019,
+    1008
+);
+
+console.log(sherlock.releaseDate);
+console.log(sherlock.state);
+sherlock.fix();
+console.log(sherlock.state);
+
+const picknick = new FantasticBook(
+    "Аркадий и Борис Стругацкие",
+    "Пикник на обочине",
+    1972,
+    168
+);
+
+console.log(picknick.author);
+picknick.state = 10;
+console.log(picknick.state);
+picknick.fix();
+console.log(picknick.state);
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value) {
+        return this.books.find(book => book[type] === value) || null;
+    }
+
+    giveBookByName(bookName) {
+        const bookIndex = this.books.findIndex(book => book.name === bookName);
+        if (bookIndex !== -1) {
+            return this.books.splice(bookIndex, 1)[0];
+        }
+        return null;
+    }
+}
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(
+    new DetectiveBook(
+        "Артур Конан Дойл",
+        "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе",
+        2019,
+        1008
+    )
+);
+library.addBook(
+    new FantasticBook(
+        "Аркадий и Борис Стругацкие",
+        "Пикник на обочине",
+        1972,
+        168
+    )
+);
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("name", "Властелин колец")); 
+console.log(library.findBookBy("releaseDate", 1924).name); 
+
+console.log("Количество книг до выдачи: " + library.books.length); 
+const borrowedBook = library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); 
+
+borrowedBook.fix();
+console.log(borrowedBook.state); 
+
+borrowedBook.fix();
+console.log(borrowedBook.state); 
+
+library.addBook(borrowedBook);
+console.log("Количество книг после добавления восстановленной книги: " + library.books.length);
+
+class Student {
+    constructor(name) {
+        this.name = name;
+        this.marks = {};
+    }
+
+    addMark(mark, subject) {
+        if (mark < 2 || mark > 5) {
+            return;
+        }
+        if (!this.marks[subject]) {
+            this.marks[subject] = [];
+        }
+        this.marks[subject].push(mark);
+    }
+
+    getAverageBySubject(subject) {
+        if (!this.marks[subject]) {
+            return 0;
+        }
+        const total = this.marks[subject].reduce((acc, mark) => acc + mark, 0);
+        return total / this.marks[subject].length;
+    }
+
+    getAverage() {
+        const subjects = Object.keys(this.marks);
+        if (subjects.length === 0) {
+            return 0;
+        }
+        const total = subjects.reduce((acc, subject) => acc + this.getAverageBySubject(subject), 0);
+        return total / subjects.length;
+    }
+}
